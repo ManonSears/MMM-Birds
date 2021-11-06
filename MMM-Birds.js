5d82f51045195a2637080eb85d82dbfd5d85732d
ManonSears <93557053+ManonSears@users.noreply.github.com>
2021-11-06 15:32:55 -0400

    Update MMM-Birds.js

--- MMM-Birds.js
+++ MMM-Birds.js
@@ -31,17 +31,20 @@ Module.register("MMM-Birds",{
     },
 
 	getDom: function() {
-    var button = document.CreatetElement("div");
+    var button = document.CreatetElement("BUTTON");
 		var iframe = document.createElement("IFRAME");
-		iframe.classList.add("iframe");
-		iframe.style = "border: 0 none transparent";
+    iframe.classList.add("iframe");
+    button.classList.add("button");
+    iframe.style = "border: 0 none transparent";
+    button.style = "border-radius: 1000px, border: 1 solid white";
+    button.width = this.config.width;
+    button.height = this.config.height;
 		iframe.width = this.config.width;
 		iframe.height = this.config.height;
 		type="text/javascript";
     iframe.src="http://kayla.manonx.com/birds.html";
 
 		// <iframe width="480" height="270" src="http://www.ustream.tv/embed/17074538?html5ui" scrolling="no" allowfullscreen webkitallowfullscreen frameborder="0" style="border: 0 none transparent;"></iframe>
-    button.className = "bird";
 
     $(button).on("click", function(){
     return iframe;
