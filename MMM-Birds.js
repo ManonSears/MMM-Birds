74e74272e18da15eff28332a066814d06e72405c
ManonSears <93557053+ManonSears@users.noreply.github.com>
2021-11-07 08:36:15 -0500

    Update MMM-Birds.js

--- MMM-Birds.js
+++ MMM-Birds.js
@@ -35,7 +35,7 @@ Module.register("MMM-Birds",{
 		var wrapper = document.createElement("div");
 		var button = document.createElement("div");
 		var text = document.createElement("span");
-		var iframe = document.createElement("IFRAME"); iframe.src = iframe.src;
+		var iframe = document.createElement("IFRAME").src += '';
 		iframe.classList.add("iframe");
 		//iframe.setAttribute("timestamp", new Date().getTime());
 		iframe.style = "border: 0 none transparent";
