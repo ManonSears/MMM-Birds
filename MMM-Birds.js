82acff8786bbbd49e2d2dad0170c07922a55e64d
ManonSears <93557053+ManonSears@users.noreply.github.com>
2021-11-06 21:57:51 -0400

    sub

--- MMM-Birds.js
+++ MMM-Birds.js
@@ -1,74 +1,52 @@
+/* global Module */
+
 /* Magic Mirror
- * Module: MMM-EARTH-Live
+ * Module: MM Hide All
  *
- * By Mykle1 - MIT Licensed
- *
+ * By EoF https://forum.magicmirror.builders/user/eof
+ * MIT Licensed.
  */
 
-Module.register("MMM-Birds",{
+Module.register("mm-hide-all",{
 
-	defaults: {
-			height:"270px",
-			width:"480px",
-      animationSpeed: "0",
-      updateInterval: 60 * 60 * 1000,
-	},
-
-    start: function () {
-		self = this;
-
-    setInterval(function() {
-    self.updateDom(self.config.animationSpeed || 0);
-    }, this.config.updateInterval);
-
-  },
-  
-  getScripts: function() {
-		return ["js/jquery.js"];
+	getScripts: function() {
+		return ["modules/mm-hide-all/js/jquery.js"];
 	},
 
 	getStyles: function() {
-        return ["css/mmm-birds.css"];
-    },
-
+		return ["mm-hide-all-style.css"];
+	},
+	
 	getDom: function() {
-    var wrapper = document.createElement("div");
+		var wrapper = document.createElement("div");
 		var button = document.createElement("div");
 		var text = document.createElement("span");
-    var iframe = document.createElement("IFRAME");
-    iframe.classList.add("iframe");
-    iframe.style = "border: 0 none transparent";
-		iframe.width = this.config.width;
-		iframe.height = this.config.height;
-		type="text/javascript";
-    iframe.src="http://kayla.manonx.com/birds.html";
-    var hidden = true;
+		var overlay = document.createElement("div");
+		var hidden = true;
 		
-
-    
-    button.className = "hide-toggle";
+		overlay.className = "paint-it-black";
+		
+		button.className = "hide-toggle";
 		button.appendChild(text);
-    text.innerHTML = "Hide";
-    
-    wrapper.appendChild(button);
-		wrapper.appendChild(iframe);
-
-      $(button).on("click", function(){
-        if(hidden){
-          $(iframe).fadeIn(1000);
-          $(button).fadeTo(1000, 0.3);
-          $(text).html('Show');
-          hidden = false;
-        }else{
-          $(iframe).fadeOut(1000);
-          $(button).fadeTo(1000, 1);
-          $(text).html('Hide');
-          hidden = true;
-        }
-      });
-      
-      return wrapper;
-
+		text.innerHTML = "Hide";
+		
+		wrapper.appendChild(button);
+		wrapper.appendChild(overlay);
+		
+		$(button).on("click", function(){
+			if(hidden){
+				$(overlay).fadeIn(1000);
+				$(button).fadeTo(1000, 0.3);
+				$(text).html('Show');
+				hidden = false;
+			}else{
+				$(overlay).fadeOut(1000);
+				$(button).fadeTo(1000, 1);
+				$(text).html('Hide');
+				hidden = true;
+			}
+		});
+		
+		return wrapper;
 	}
-
 });
