95ea9c96ab07a01db0240b89a3608ea20d0a794e
ManonSears <93557053+ManonSears@users.noreply.github.com>
2021-11-06 21:54:51 -0400

    sub

--- MMM-Birds.js
+++ MMM-Birds.js
@@ -33,7 +33,8 @@ Module.register("MMM-Birds",{
 
 	getDom: function() {
     var wrapper = document.createElement("div");
-    var button = document.createElement("BUTTON");
+		var button = document.createElement("div");
+		var text = document.createElement("span");
     var iframe = document.createElement("IFRAME");
     button.classList.add("button");
     button.style = "border-radius: 1000px, border: 1 solid white";
@@ -43,13 +44,32 @@ Module.register("MMM-Birds",{
 		iframe.height = this.config.height;
 		type="text/javascript";
     iframe.src="http://kayla.manonx.com/birds.html";
+    var hidden = true;
 		
-		wrapper.appendChild(button);
-		wrapper.appendChild(iframe);
-		
-	
-      return iframe;
 
+    
+    button.className = "hide-toggle";
+		button.appendChild(text);
+    text.innerHTML = "Hide";
+    
+    wrapper.appendChild(button);
+		wrapper.appendChild(iframe);
+
+      $(button).on("click", function(){
+        if(hidden){
+          $(iframe).fadeIn(1000);
+          $(button).fadeTo(1000, 0.3);
+          $(text).html('Show');
+          hidden = false;
+        }else{
+          $(iframe).fadeOut(1000);
+          $(button).fadeTo(1000, 1);
+          $(text).html('Hide');
+          hidden = true;
+        }
+      });
+      
+      return wrapper;
 
 	}
 
