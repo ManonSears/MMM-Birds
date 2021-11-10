cb6a56b5209dc160fed6185654863ca7b667fc39
ManonSears <93557053+ManonSears@users.noreply.github.com>
2021-11-09 20:31:15 -0500

    Update MMM-Birds.js

--- MMM-Birds.js
+++ MMM-Birds.js
@@ -38,7 +38,8 @@ Module.register("MMM-Birds",{
 		var iframe = document.createElement("IFRAME");
 		iframe.classList.add("iframe");
 		iframe.style = "border: 0 none transparent";
-		iframe.style.height = '100%';
+		iframe.style.height = '100vh';
+		wrapper.style.height = '100vh';
 		iframe.width = this.config.width;
 		type="text/javascript";
 		iframe.src="http://kayla.manonx.com/birds.html";
